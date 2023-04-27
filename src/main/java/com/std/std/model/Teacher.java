7f225a337d9664ad573452ed681de65782bc1e1a
slydesigns <sylvestorsielei@gmail.com>
2023-04-27 10:38:36 +0300

    Teacher

---
+++ src/main/java/com/std/std/model/Teacher.java
@@ -0,0 +1,51 @@
+package com.std.std.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "teacher")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    public Teacher() {
+    }
+
+    public Teacher(String name, Department department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
